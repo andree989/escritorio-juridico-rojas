80fd0944780a011635f10332589603ce49341273
Andreina Rojas <andre_is89@hotmail.com>
2026-03-25 21:40:17 -0300

    Refactor: Remove unused Resources component and clean up imports in main.tsx

--- src/components/LegalNewsAndResources.tsx
+++ src/components/LegalNewsAndResources.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "../styles.css";
 import { FaNewspaper, FaGavel, FaBook, FaFileAlt, FaUserTie, FaBriefcase } from "react-icons/fa";
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import LegalNewsAndResources from './components/LegalNewsAndResources';
-import Resources from './components/Resources';
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App>
-      <LegalNewsAndResources />
-      <Resources />
     </App>
   </StrictMode>,
 )
